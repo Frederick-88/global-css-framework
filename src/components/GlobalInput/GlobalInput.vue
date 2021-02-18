2e9e07eaf381b0e1d94a9ada05da00f14336238d
Frederick <frederickfd88@gmail.com>
2021-02-18 21:58:49 +0700

    Done GlobalInput, left icon integrations

--- src/components/GlobalInput/GlobalInput.vue
+++ src/components/GlobalInput/GlobalInput.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="input-field__wrapper">
+  <div class="global-input__wrapper" :class="[{ 'has-label': label !== '' }]">
+    <template v-if="label">
+      <label>{{ label }}</label>
+    </template>
     <input
       :class="inputClassList"
+      :type="type"
       :disabled="isDisabled"
       :value="value"
       :placeholder="placeholder"
@@ -13,6 +17,21 @@
       @keyup="$emit('keyup', $event)"
       @keypress.enter="$event.target.blur"
     />
+
+    <template v-if="(message && isSuccess) || isWarning || isError">
+      <div :class="messageClassList">
+        <template v-if="isSuccess">
+          <i class="icon icon-tick-solid"></i>
+        </template>
+        <template v-else-if="isWarning">
+          <i class="icon icon-alert"></i>
+        </template>
+        <template v-else-if="isError">
+          <i class="icon icon-close-solid"></i>
+        </template>
+        <span v-html="message"></span>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -20,18 +39,40 @@
 export default {
   name: "GlobalInput",
   props: {
+    type: { type: String, default: "" },
+    label: { type: String, default: "" },
     placeholder: { type: String, default: "" },
-    isDisabled: { type: Boolean, default: false },
     value: { type: String, default: "" },
+    message: { type: String, default: "" },
+
+    isDisabled: { type: Boolean, default: false },
     isReadOnly: { type: Boolean, default: false },
+    isTall: { type: Boolean, default: false },
+    isShort: { type: Boolean, default: false },
+    isWarning: { type: Boolean, default: false },
+    isSuccess: { type: Boolean, default: false },
+    isError: { type: Boolean, default: false },
   },
 
   computed: {
     inputClassList() {
-      const classList = ["input-field"];
+      const classList = ["global-input"];
 
-      if (this.isTall) classList.push("input-field--tall");
-      if (this.isShort) classList.push("input-field--short");
+      if (this.isTall) classList.push("global-input--tall");
+      if (this.isShort) classList.push("global-input--short");
+      if (this.isSuccess) classList.push("global-input--success");
+      if (this.isError) classList.push("global-input--error");
+      if (this.isWarning) classList.push("global-input--warning");
+
+      return classList;
+    },
+
+    messageClassList() {
+      const classList = ["global-input__message"];
+
+      if (this.isSuccess) classList.push("is-success");
+      if (this.isWarning) classList.push("is-warning");
+      if (this.isError) classList.push("is-error");
 
       return classList;
     },
@@ -52,23 +93,36 @@ export default {
 </script>
 
 <style lang="scss">
-.input-field__wrapper {
+.global-input__wrapper {
   position: relative;
 
-  .input-field {
+  &.has-label {
+    margin-bottom: 10px;
+
+    label {
+      color: $grey;
+      margin-bottom: 5px;
+      display: block;
+      font-size: 0.875rem;
+      float: left;
+    }
+  }
+
+  .global-input {
     display: inline-block;
+    width: 100%;
     padding: 10px 15px;
-    border-radius: 8px;
-    border: 1px solid $primary2;
+    border-radius: 4px;
+    border: 1px solid $disabledGrey;
     font-family: "Work Sans", "Arial", "Helvetica", "sans-serif";
     font-weight: 500;
-    font-size: 0.875em;
-    color: #000000;
+    font-size: 0.875rem;
+    color: $black;
     margin: 0;
     outline: 0;
 
     &:disabled {
-      color: #ffffff;
+      color: $lightGrey5;
     }
 
     &:focus {
@@ -76,13 +130,55 @@ export default {
     }
   }
 
-  .input-field--tall {
+  .global-input--success {
+    border: 1px solid $success1;
+  }
+
+  .global-input--error {
+    border: 1px solid $danger1;
+  }
+
+  .global-input--warning {
+    border: 1px solid $warning3;
+  }
+
+  .global-input--tall {
     padding-top: 12px;
     padding-bottom: 12px;
   }
 
-  .input-field--short {
+  .global-input--short {
     padding: 8px 15px;
+  }
+
+  .global-input__message {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    position: absolute;
+    font-size: 0.75rem;
+    width: 100%;
+    background: $white;
+    box-shadow: 0 3px 4px rgba(10, 31, 68, 0.1);
+    padding: 10px;
+    z-index: 1;
+
+    .icon {
+      margin-right: 10px;
+      font-size: 1.25rem;
+    }
+
+    &.is-success {
+      color: $success1;
+    }
+
+    &.is-warning {
+      color: $warning3;
+    }
+
+    &.is-error {
+      color: $danger1;
+    }
   }
 }
 </style>
