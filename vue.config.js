7ebb67c94ec67ec0be8298e4c536a54ec512d8ed
FrederickOffeo <frederick@offeo.com>
2021-02-23 21:57:06 +0700

    added note for vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,18 @@ module.exports = {
     loaderOptions: {
       sass: {
         // method of SCSS to use variables globally
+        /**
+         * Warning!
+         *
+         * Do not import actual styles
+         *
+         * Only import variables, mixins, function because they won't appear in actual build
+         * Importing style will include them in every components
+         *
+         */
         data: `
           @import '@/assets/styles/variables.scss';
+          @import '@/assets/icomoon/variables.scss';
         `,
       },
     },
