cbb8f79edef1de8b69b66c0bd16b8aa856397488
FrederickOffeo <frederick@offeo.com>
2021-03-20 16:41:53 +0700

    Add GlobalCheckboxList for Storybook Needs

---
+++ src/components/GlobalCheckbox/GlobalCheckboxList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="global-checkbox__lists">
+    <GlobalCheckbox
+      class="checkbox"
+      v-for="(checkbox, index) in checkboxLists"
+      :key="index"
+      :label="checkbox.label"
+      :is-checked="checkbox.isChecked"
+      :is-primary="checkbox.isPrimary"
+      :is-success="checkbox.isSuccess"
+      :is-warning="checkbox.isWarning"
+      :is-danger="checkbox.isDanger"
+    />
+  </div>
+</template>
+
+<script>
+import GlobalCheckbox from "./GlobalCheckbox";
+export default {
+  name: "GlobalCheckboxList",
+  components: { GlobalCheckbox },
+  props: {
+    checkboxLists: { type: Array, required: true, default: () => [] },
+  },
+};
+</script>
+
+<style lang="scss">
+.global-checkbox__lists {
+  .checkbox {
+    margin-bottom: 10px;
+  }
+}
+</style>
