47426930273473f129d55d9c60021b4c66edae5b
Chen Frederick <frederickfd88@gmail.com>
2021-05-23 14:13:12 +0700

    updated basedialogprops section message in documentation

--- src/stories/BaseDialogProps.stories.js
+++ src/stories/BaseDialogProps.stories.js
@@ -9,7 +9,7 @@ const Template = (args) => ({
   props: Object.keys(args),
   components: { GlobalBaseDialog },
   template: `<div>
-      <h4>This is the props section, no need to follow the code snippet here.</h4> 
+      <h4>This is the props section, you don't need to follow the code snippet here.</h4> 
       <GlobalBaseDialog v-bind="$props" :is-show="false" />
     </div>`,
 });
